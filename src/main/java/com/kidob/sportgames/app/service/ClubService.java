38914a16238879beeb1d08d185c5a703fa58ff42
BogdanMateratskyy <bogdan.materatskyy@gmail.com>
2019-05-18 17:35:32 +0300

    Created methods for implements CRUD operations over club entities

--- src/main/java/com/kidob/sportgames/app/service/ClubService.java
+++ src/main/java/com/kidob/sportgames/app/service/ClubService.java
@@ -1,5 +1,11 @@
 package com.kidob.sportgames.app.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.kidob.sportgames.app.model.entity.sport.Team;
+import com.kidob.sportgames.app.rest.dto.TeamDTO;
+
 /**
  *  Entry point to perform operations over club entities
  * 
@@ -7,5 +13,36 @@ package com.kidob.sportgames.app.service;
  *
  */
 public interface ClubService {
+	
+	/**
+	 * Saves specified club instance
+	 
+	 * @param team
+	 * @return the saved {@link Team} entity will never be null
+	 */
+	Team saveTeam(Team team);
 
+	/**
+	 * Updates specified club instance if exists
+	 * 
+	 * @param team
+	 * @return
+	 */
+	Team updateTeam(TeamDTO team);
+
+	/**
+	 * Return list of existing clubs
+	 * 
+	 * @return
+	 */
+	List<Team> findTeams();
+
+	/**
+	 * Returns club with specified identifier. If club isn't found then empty
+	 * optional is returned.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	Optional<Team> findTeamById(Long teamId);
 }
